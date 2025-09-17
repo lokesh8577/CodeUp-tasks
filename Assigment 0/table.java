568bfbcdcb58e32623f34e49c3593547675fa6b9
Lokesh Prajapat <165503393+lokesh8577@users.noreply.github.com>
2025-09-17 12:17:57 +0530

    Add files via upload

---
+++ Assigment 0/table.java
@@ -0,0 +1,32 @@
+//import scanner class which is use to take input from user
+import java.util.Scanner;
+
+
+//create class
+public class table{
+
+//main function
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter a number which table you want to print : ");
+
+//variable declare and taking input
+int n = sc.nextInt();;
+
+//fuunction calling
+print_table(n);
+}
+
+
+
+//function which print table
+public static void print_table(int n){
+
+//using for loop for print the table from 1 to 10
+for(int i=1;i<=10;i++){
+System.out.println(n+"*"+i+"="+n*i);
+}
+}
+}
