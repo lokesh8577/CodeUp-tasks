ed717f0030c4b7be6cf54442ec36e7d2a564514f
Lokesh Prajapat <165503393+lokesh8577@users.noreply.github.com>
2025-09-24 11:29:18 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,16 @@
+public class Constant {
+    public static final String OP_STRING = """
+            1. Expand Characters
+            2. Character Frequency
+            3. Check Prime Number
+            4. Number to Words
+            5. Longest Unique Substring Length
+            6. Exit
+            Enter your choice:""";
+    public static final String ENTER_STRING = "Enter String: ";
+    public static final String ENTER_NUMBER = "Enter Number: ";
+    public static final String INVALID_INPUT = "Invalid input. Please try again.";
+    public static final String EXIT__STRING = "Exiting program...";
+    public static final String PRIME_STRING = " is a prime number.";
+    public static final String NOT_PRIME_STRING = " is not a prime number.";
+}
